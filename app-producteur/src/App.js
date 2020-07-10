1fa9659ecd59144626fc595fdd241ed4497e2af6
Mariem ABICHOU <mariemabichou1710@gmail.com>
2020-07-10 18:10:27 +0200

    modif header footer index

--- app-producteur/src/App.js
+++ app-producteur/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Footer from './components/Footer.jsx'
+import Header from './components/Header.jsx'
+import Home from './pages/Home.jsx'
 function App() {
   return (
     <div className="App">
-     
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
     </div>
+    
   );
 }
 
--- app-producteur/src/pages/Home.jsx
+++ app-producteur/src/pages/Home.jsx
@@ -1,1 +1,27 @@
-<p>Page Home</p>
+import React, { Component } from "react"
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+        console.log("Constructeur appelé")
+        this.state = {
+        }
+    }
+    render() {
+        console.log("render appelé")
+        return (
+            <>
+                <p>Page Home</p>
+                <h1>Bienvenue chez-mon-producteur.com ! </h1>
+                <p>Trouver des produits locaux à coté de chez vous</p>
+                <ul>
+                    <li>Produit garanti 100% frais</li>
+                    <li>Promouvoir l'agriculture locale</li>
+                    <li>Politique anti-gaspillage</li>
+                </ul>
+            </>
+        )
+    }
+}
+
+export default Home
